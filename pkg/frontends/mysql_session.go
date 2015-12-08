1529f2cde39213e2ea18022edea22f6bebb27c98
Aaron Raddon <aaron@lytics.io>
2015-12-07 22:06:51 -0800

    add internal vars to session

--- pkg/frontends/mysql_session.go
+++ pkg/frontends/mysql_session.go
@@ -2,9 +2,11 @@ package frontends
 
 import (
 	"github.com/araddon/qlbridge/datasource"
+	"github.com/araddon/qlbridge/value"
 )
 
 func NewMysqlSession() *datasource.ContextSimple {
 	ctx := datasource.NewContextSimple()
+	ctx.Data["@@max_allowed_packet"] = value.NewIntValue(MaxAllowedPacket)
 	return ctx
 }
